09efe5eb0d91eebfbda90c2a4b4de4c67c3b02b3
Gloozzyy <75518376+Gloozzyy@users.noreply.github.com>
2021-12-05 15:56:13 -0800

    Update Universal.lua

--- ViiPer Hub/Universal.lua
+++ ViiPer Hub/Universal.lua
@@ -1,3 +1,6 @@
+-- Gui to Lua
+-- Version: 3.2
+
 -- Instances:
 
 local ViiPerGlobals = Instance.new("ScreenGui")
@@ -12,10 +15,12 @@ ViiPerGlobals.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 Frame.Parent = ViiPerGlobals
 Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.Position = UDim2.new(0.158343479, 0, 0.0989272967, 0)
 Frame.Size = UDim2.new(0, 100, 0, 100)
 
 TextButton.Parent = ViiPerGlobals
 TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton.Position = UDim2.new(0.158343479, 0, 0.0989272967, 0)
 TextButton.Size = UDim2.new(0, 200, 0, 50)
 TextButton.Font = Enum.Font.SourceSans
 TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
